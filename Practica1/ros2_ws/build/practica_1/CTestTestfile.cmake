2a7fc000481ef01077840193b47238dfca0eaf82
sr-gus <lgmarin02@hotmail.com>
2025-04-27 08:51:19 -0600

    feat(practicas,examenes): Avances en práctica 1 y Examen Parcial 2

---
+++ Practica1/ros2_ws/build/practica_1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robousr/Robotica2025-2/robolab20252/Practica1/ros2_ws/practica_1
+# Build directory: /home/robousr/Robotica2025-2/robolab20252/Practica1/ros2_ws/build/practica_1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Practica1/ros2_ws/practica_1/launch/robot_prismatic_launch.py
@@ -0,0 +1,28 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('practica_1')
+    urdf_file = os.path.join(pkg_share, 'urdf', 'robot_prismatic.urdf')
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': Command(['xacro ', urdf_file])}]
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        robot_state_publisher,
+        rviz_node
+    ])
